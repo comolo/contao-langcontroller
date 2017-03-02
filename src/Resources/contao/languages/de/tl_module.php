fc1e48dfc7f6f3564fa1ae249ca9af282963b19b
Hendrik Obermayer <h.obermayer@comolo.de>
2017-03-02 17:06:07 +0100

    added translation

---
+++ src/Resources/contao/languages/de/tl_module.php
@@ -0,0 +1,5 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_module']['languageModuleMapping'] = ['Zuordnung', 'Die Module einer Sprache zuordnen'];
+$GLOBALS['TL_LANG']['tl_module']['languageModuleMapping']['languageCode'] = ['Sprache'];
+$GLOBALS['TL_LANG']['tl_module']['languageModuleMapping']['module'] = ['Modul'];
